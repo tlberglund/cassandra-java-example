51d9337e1bbb13bd270c90c8c2f485a8579a7241
Tim Berglund <tlberglund@gmail.com>
2017-06-16 09:36:16 -0700

    Prepared Statement

--- src/main/java/Cassandra.java
+++ src/main/java/Cassandra.java
@@ -1,19 +1,27 @@
 import com.datastax.driver.core.*;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.UUID;
 
 
 public class Cassandra {
   
+  
+  public void insertMovie(Session session, String title, int releaseYear) {
+    PreparedStatement insertSmt = session.prepare("INSERT INTO movies (movie_id, title, release_year) VALUES (?,?,?);");
+    BoundStatement bs = insertSmt.bind(UUID.randomUUID(), title, releaseYear);
+    session.execute(bs);
+  }
+
   public static void main(String args[]) {
+    Cassandra c = new Cassandra();
     Cluster cluster;
     Session session;
     cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
     session = cluster.connect("essentials");
 
-    session.execute("INSERT INTO movies (movie_id, title, release_year) VALUES (uuid(), 'Blade Runner 2049', 2017)");
+    // session.execute("INSERT INTO movies (movie_id, title, release_year) VALUES (uuid(), 'Blade Runner 2049', 2017)");
 
+    c.insertMovie(session, "Real Genius", 1985);
+    
     ResultSet results = session.execute("SELECT * FROM movies");
 
     for (Row row : results) {
