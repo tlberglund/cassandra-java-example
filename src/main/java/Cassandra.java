e95583dad61cb8a5eec323fe1c4e4457f4665b6b
Tim Berglund <tlberglund@gmail.com>
2017-06-16 08:45:36 -0700

    Very simple query

--- src/main/java/Cassandra.java
+++ src/main/java/Cassandra.java
@@ -1,8 +1,24 @@
+import com.datastax.driver.core.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.UUID;
 
 
 public class Cassandra {
   
   public static void main(String args[]) {
-    
+    Cluster cluster;
+    Session session;
+    cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
+    session = cluster.connect("essentials");
+
+    session.execute("INSERT INTO movies (movie_id, title, release_year) VALUES (uuid(), 'Blade Runner 2049', 2017)");
+
+    ResultSet results = session.execute("SELECT * FROM movies");
+
+    for (Row row : results) {
+       System.out.format("%s %s %s\n", row.getUUID("movie_id"), row.getString("title"),row.getInt("release_year"));
+    }
+
   }
 }
